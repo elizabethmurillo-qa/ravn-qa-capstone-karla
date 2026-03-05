1932c2db2ee0851d265ffa7248270a38e6dbbe7b
elizabethmurillo-qa <elizabethmurillo@ravn.co>
2026-03-05 07:58:57 -0600

    add testData file with valid and invalid data to test

---
+++ fixtures/testData.js
@@ -0,0 +1,29 @@
+const TOPICS = {
+    GENERAL: "General Inquiry",
+    FEATURE: "Feature Request",
+    BUG: "Bug Report",
+    SUPPORT: "Support",
+    OTHER: "Other"
+}
+//Valid Data
+const validData = {
+    name: "karla",
+    email: "karla@gmail.com",
+    topic: TOPICS.GENERAL,
+    message: "This is a test with valid data"
+    
+}
+
+//Invalid Data
+const fieldsEmtpy = { name: "", email: "", topic: "", message: ""} //fields empty 
+const fieldsWithWhitespaces = { name: " ", email: " ", message: " "} //fields with whitespaces
+const fieldNameWithNumbers = { name: "2", email: "karla@gmail.com", topic: TOPICS.SUPPORT, message: "This is a test with numbers"} //name with numbers
+const invalidEmailFormat = { name: "karla", email: "karlagmail.com", topic: TOPICS.BUG, message: "This a test with invalid email"} //INVALID EMAIL FORMAT
+const invalidNameLength = { name: "karlaelizabethmurillourrutiakarlaelizabethmurillourrutia", 
+                            email: "karla@gamil.com",
+                            topic: TOPICS.FEATURE,  
+                            message: "This is a test with invalid length "
+                        }
+
+module.exports = {TOPICS, validData, fieldsEmtpy, fieldsWithWhitespaces, fieldNameWithNumbers, invalidEmailFormat, invalidNameLength}
+
