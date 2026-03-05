636718577b4b5b782c3785899e1eee1e0946082c
elizabethmurillo-qa <elizabethmurillo@ravn.co>
2026-03-05 04:16:38 -0600

    add setup: structure, playwright config and BasePage

---
+++ pages/BasePage.js
@@ -0,0 +1,36 @@
+const { expect } = require("@playwright/test");
+
+class BasePage {
+    constructor(page) {
+        this.page = page;
+    }
+
+    async navigateTo(url) {
+        await this.page.goto(url);
+    }
+
+    async waitForElement(locator) {
+        await locator.waitFor({ state: "visible"})
+    }
+
+    async clickElement(locator) {
+        await this.waitForElement(locator);
+        await locator.click();
+    }
+
+    async fillFields(locator, value) {
+        await locator.clear();
+        await locator.fill(value);
+    }
+
+    async isVisible(locator) {
+        return await locator.isVisible()
+    }
+
+    async scrollToElement(locator) {
+        await locator.scrollIntoViewIfNeeded()
+    }
+
+}
+
+module.exports = {BasePage}
