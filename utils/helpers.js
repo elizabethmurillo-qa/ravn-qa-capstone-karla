fe9f577a55ce07b7fb0e5937ce3a6d56ea73edda
elizabethmurillo-qa <elizabethmurillo@ravn.co>
2026-03-10 07:05:46 -0600

    add functions for auto data generation to helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,27 @@
+const { faker } = require("@faker-js/faker")
 const skipIfNot = (test, ...allowedProjects) => {
   const projectName = test.info().project.name;
   test.skip(!allowedProjects.includes(projectName),
     `Skipped: only runs on ${allowedProjects.join(", ")}`);
 };
 
-module.exports = { skipIfNot };
+function generateName() {
+  return faker.person.firstName()
+}
+
+function generateEmail() {
+  return faker.internet.email()
+}
+
+function generateMessage() {
+  return faker.lorem.sentence()
+}
+
+function generateLongName() {
+  return faker.string.alpha(51)
+}
+
+function generateNameNumber() {
+  return faker.number.int({min: 1, max: 9}).toString();
+}
+module.exports = { skipIfNot, generateName, generateEmail, generateMessage, generateLongName, generateNameNumber };
