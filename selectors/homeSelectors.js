905096403104253afa265c8101cd5af80a12659c
elizabethmurillo-qa <elizabethmurillo@ravn.co>
2026-03-11 21:39:25 -0600

    adding new selector on homeSelectors - Accesibility testing

--- selectors/homeSelectors.js
+++ selectors/homeSelectors.js
@@ -2,8 +2,9 @@
 const homeSelectors = {
         mainMenu: (page) => page.getByRole("navigation", { name: "Main navigation" }),
         contactLink: (page) => homeSelectors.mainMenu(page).getByRole("link", { name: "Contact" }),
-        contactSection: (page) => page.getByRole('heading', { name: 'Get in Tuch' })
-        //contactSection: (page) => page.getByRole("heading", {name: "Get in Tuch"})
+        contactSection: (page) => page.getByRole('heading', { name: 'Get in Tuch' }),
+        contactA11y: '#contact'
+        
 }
 
 export { homeSelectors }
