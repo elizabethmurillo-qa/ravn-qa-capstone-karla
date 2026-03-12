e8fc7e76ebc73affd15c3422bc761650d82216bf
elizabethmurillo-qa <elizabethmurillo@ravn.co>
2026-03-11 21:42:32 -0600

    adding new test suite: Accesibility testing for Contact Section

---
+++ tests/contact-accesibility.spec.js
@@ -0,0 +1,103 @@
+import { test, expect}  from "@playwright/test";
+import { homeSelectors } from "../selectors/homeSelectors.js";
+//import { contactSelectors } from "../selectors/contactSelectors.js";
+import AxeBuilder from "@axe-core/playwright";
+import { PageManager } from "../pages/PageManager.js";
+import { skipIfNot } from "../utils/helpers.js" ;
+import { a11yFinalReport, allResultsTests, } from "../utils/helpers.js";
+import {
+  validData, fieldsEmtpy, fieldsWithWhitespaces, fieldNameWithNumbers, invalidEmailFormat, invalidNameLength
+} from "../data/testData.js";
+import { contactSelectors } from "../selectors/contactSelectors.js";
+
+let pageManager, home, contact;
+
+test.beforeEach( async ({ page}) => {
+    skipIfNot(test, "chromium")
+    pageManager = new PageManager(page);
+    home = pageManager.getHomePage();
+    contact = pageManager.getContactPage();
+    await home.open(page);
+    await home.goToContactSection(page);
+    
+    
+  });
+
+    test("Test-001: Contact form mush use valid ARIA attributes", async ({page}) => {
+        const a11y = await new AxeBuilder({page})
+        .include(homeSelectors.contactA11y)
+        .withRules([
+            'aria-valid-attr','aria-required-attr', 'aria-allowed-attr'
+        ]).analyze()
+
+        allResultsTests(a11y)
+        expect(a11y.violations).toEqual([])
+    })
+
+    test("Test-002: Contact form inputs and button must have labels", async ({page}) => {
+        const a11y = await new AxeBuilder({page})
+        .include(homeSelectors.contactA11y)
+        .withRules([
+            'label', 'aria-input-field-name', 'button-name'
+        ]).analyze()
+
+        allResultsTests(a11y)
+        expect(a11y.violations).toEqual([])
+    }) 
+
+    test("Test-003: Contact Form fields must not have multiple labels", async ({page}) => {
+      const a11y = await new AxeBuilder({page})
+        .include(homeSelectors.contactA11y)
+        .withRules([
+            'form-field-multiple-labels'
+        ]).analyze()
+
+        allResultsTests(a11y)
+        expect(a11y.violations).toEqual([])
+    })
+
+   test("Test-004: Focus visible", async ({page}) => {
+      const a11y = await new AxeBuilder({page})
+        .include(homeSelectors.contactA11y)
+        .withRules([
+            'scrollable-region-focusable',  'focus-order-semantics'
+        ]).analyze()
+
+        allResultsTests(a11y)
+        expect(a11y.violations).toEqual([])
+    })
+
+    test("Test-005: Contact form must meet color contrast and focus accesibility", async({page}) => {
+      
+      const a11y = await new AxeBuilder({page})
+      .include(homeSelectors.contactA11y)
+      .withRules([
+        'color-contrast'
+      ]).analyze();
+
+    
+      allResultsTests(a11y)
+      expect(a11y.violations).toEqual([])
+    })
+
+
+    test.afterAll(() => {
+      a11yFinalReport()    
+  })
+
+    /* test("Test-006: all", async ({page}) => {
+      const a11y = await new AxeBuilder({page})
+        .include(homeSelectors.contactA11y)
+        .withTags([
+            'wcag2a', 'wcag2aa'
+        ]).analyze()
+
+        allResultsTests(a11y)
+        expect(a11y.violations).toEqual([])
+    })  */
+
+    
+ 
+
+
+
