bbcf19edfd3ea04eaa8b7ee811235ce30c637360
elizabethmurillo-qa <elizabethmurillo@ravn.co>
2026-03-19 14:20:33 -0600

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -104,7 +104,7 @@ export async function waitForPageReady(page) {
 export function compareOptions(overrides = {}) {
   return screenshotOptions({
     threshold: 0.2,
-    maxDiffPixelsRatio: 0.03,
+    maxDiffPixelRatio: 0.03,
   ...overrides,
   })
 }
